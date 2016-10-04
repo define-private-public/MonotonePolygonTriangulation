a6918abe7a3e073ecb90414a9a4fa150b1075af8
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-10-03 21:16:13 -0400

    working on the actual code

--- triangulate.dart
+++ triangulate.dart
@@ -6,16 +6,23 @@ import 'merge-sort.dart';
 import 'comparable-point.dart';
 
 
-// Makes a nice RGB values
-String rgb(int r, int g, int b) => 'rgb($r, $g, $b)';
+// Makes a nice RGB value
+String rgb(int r, int g, int b) =>
+  'rgb($r, $g, $b)';
+
+// Some constants
+string backgroundColor = rgb(0, 46, 76);
+
+
+// Interactive section
+CanvasElement canvas = querySelector('#polygon-canvas');
+CanvasRenderingContext2D ctx = canvas.context2D;
+
 
 
 void main() {
-  CanvasElement canvas = querySelector('#polygon-canvas');
-
   // Fill the background
-  CanvasRenderingContext2D ctx = canvas.context2D;
-  ctx..fillStyle = rgb(0, 46, 76)
+  ctx..fillStyle = backgroundColor
      ..fillRect(0, 0, canvas.width, canvas.height);
 }
 
